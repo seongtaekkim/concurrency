896b204efb65fe85c1b9e82a00f3ab21b01ca71c
seongtaekkim <chxortnl@gmail.com>
2023-12-29 20:59:39 +0900

    Test: ADD Parallel가 필요한 경우

---
+++ concurrency-practice/src/main/java/me/staek/os/Parallelism.java
@@ -0,0 +1,40 @@
+package me.staek.os;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Parallel은 cpu연산을 주로 할 때 코어 개수보다 작업 스레드가 같거나 작아야
+ * 가장 큰 효과가 있다.
+ * - context switching 을 안해도 되기 때문.
+ *
+ */
+public class Parallelism {
+    public static void main(String[] args) {
+        int cpuCores = 1;
+//        int cpuCores = Runtime.getRuntime().availableProcessors();
+
+        List<Integer> data = new ArrayList<>();
+        for (int i = 0; i < cpuCores; i++) {
+            data.add(i);
+        }
+
+        long startTime1 = System.currentTimeMillis();
+        long sum1 = data.parallelStream()
+                .mapToLong(i -> {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return i * i;
+                })
+                .sum();
+
+        long endTime1 = System.currentTimeMillis();
+
+        System.out.println("걸린 시간: " + (endTime1 - startTime1) + "ms");
+        System.out.println("결과1: " + sum1);
+
+    }
+}
