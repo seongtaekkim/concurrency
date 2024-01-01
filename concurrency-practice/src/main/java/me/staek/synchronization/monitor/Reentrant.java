39f5c0decf6773f33a901c671692859c6205c8d3
seongtaekkim <chxortnl@gmail.com>
2024-01-01 23:27:37 +0900

    Test: ADD 상속관계의 객체의 모니터 락 테스트

---
+++ concurrency-practice/src/main/java/me/staek/synchronization/monitor/Reentrant.java
@@ -0,0 +1,39 @@
+package me.staek.synchronization.monitor;
+
+class Parent {
+    public synchronized void parentMethod() {
+        System.out.println("call Parent method");
+    }
+}
+
+class Child extends Parent {
+
+    public synchronized void childMethod() {
+        System.out.println("call Child method");
+        parentMethod();
+    }
+}
+
+/**
+ * 상속관계의 객체의 모니터 락은 같다.
+ * 자바의 같은 모니터 락은 재진입이 가능하므로,
+ * 예제와 같이 하위 객체의 모니터 내부에서 부모의 모니터 메서드를 호출하여 재진입 할 수 있따.
+ */
+public class Reentrant {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+
+        Thread t1 = new Thread(() -> {
+            child.childMethod();
+        });
+
+        t1.start();
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
