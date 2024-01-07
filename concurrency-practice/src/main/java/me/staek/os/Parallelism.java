b31bf410890ac62f24d7b2bc5ad1dcfc00f67056
seongtaekkim <chxortnl@gmail.com>
2024-01-07 12:51:45 +0900

    Comment: MOD

--- concurrency-practice/src/main/java/me/staek/os/Parallelism.java
+++ concurrency-practice/src/main/java/me/staek/os/Parallelism.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 병렬성은 CPU 가 동시에 많은 일을 수행하는 것에 중점을 둔다. 즉 CPU 가 놀지 않고 최대한 바쁘게 동작해야 한다
+ *
  * Parallel은 cpu연산을 주로 할 때 코어 개수보다 작업 스레드가 같거나 작아야
  * 가장 큰 효과가 있다.
  * - context switching 을 안해도 되기 때문.
