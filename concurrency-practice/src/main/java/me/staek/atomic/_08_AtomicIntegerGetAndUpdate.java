0cde3ce40e881de21b6c53fb2bdaddd378321e97
seongtaekkim <chxortnl@gmail.com>
2024-01-08 20:47:45 +0900

    Test: MOD add synchronized ex

--- concurrency-practice/src/main/java/me/staek/atomic/_08_AtomicIntegerGetAndUpdate.java
+++ concurrency-practice/src/main/java/me/staek/atomic/_08_AtomicIntegerGetAndUpdate.java
@@ -10,8 +10,9 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class _08_AtomicIntegerGetAndUpdate {
     private static AtomicInteger account = new AtomicInteger(1000);
+    private static int account2 = 1000;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 5; i++) {
             new Thread(() -> {
                 int withdrawalAmount = 500;
@@ -23,6 +24,32 @@ public class _08_AtomicIntegerGetAndUpdate {
                     }
                 });
                 if (updatedBalance == -1) {
+                    System.out.println(Thread.currentThread().getName() + " : 잔고부족: " + account.get());
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " : 출금 후 잔고: " + account.get());
+                }
+            }).start();
+
+        }
+        Thread.sleep(1000);
+        System.out.println("====================================");
+        System.out.println("====================================");
+        System.out.println("====================================");
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                int withdrawalAmount = 500;
+                int updatedBalance = 0;
+                synchronized (_08_AtomicIntegerGetAndUpdate.class) {
+                    if (account2 >= withdrawalAmount) {
+                        updatedBalance = account2 - withdrawalAmount; // 출금 성공
+                        account2 = updatedBalance;
+                    } else {
+                        updatedBalance = -1;
+                    }
+                }
+
+                if (updatedBalance < 0) {
                     System.out.println(Thread.currentThread().getName() + " : 잔고부족: " + updatedBalance);
                 } else {
                     System.out.println(Thread.currentThread().getName() + " : 출금 후 잔고: " + updatedBalance);
