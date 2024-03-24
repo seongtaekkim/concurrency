cdfff49318d258f6763370dcb4b1d6ef1c9f6f8a
seongtaekkim <chxortnl@gmail.com>
2024-03-24 12:13:58 +0900

    Test: MOD 테스트 수정

--- concurrency-practice/src/main/java/me/staek/threadpool/future/_04_CallbackWithFuture.java
+++ concurrency-practice/src/main/java/me/staek/threadpool/future/_04_CallbackWithFuture.java
@@ -6,7 +6,7 @@ import java.util.concurrent.*;
  * ExecutorService - future + callback
  *
  * - Future는 get() 때문에 sync 처럼 동작한다.
- * - 새로운 스레드에서 Future get()을 기다리고, 결과를 인자로 보낸 콜백클래스에서 받아수행한다면
+ * - 새로운 스레드에서 Future get()을 기다리고, 결과를 인자로 보낸 콜백클래스에서 받아 수행한다면
  *   async형태로 동작하게 할 수 있을 것이다.
  */
 public class _04_CallbackWithFuture {
