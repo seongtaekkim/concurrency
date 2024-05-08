7201251aed443658f990503e25672749b410e62c
seongtaekkim <chxortnl@gmail.com>
2024-05-08 09:55:20 +0900

    completablefuture - ThenRun

--- concurrency-practice/src/main/java/me/staek/nonblocking/completablefuture/_06_ThenRun.java
+++ concurrency-practice/src/main/java/me/staek/nonblocking/completablefuture/_06_ThenRun.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 /**
- *
+ * completablefuture - 06_ThenRun
  */
 public class _06_ThenRun {
     public static void main(String[] args) {
