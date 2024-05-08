d0ae7ee6db2cb221177fe0ddb3beda37370621fe
seongtaekkim <chxortnl@gmail.com>
2024-05-08 10:02:36 +0900

    complete - CompleteExceptionally

---
+++ concurrency-practice/src/main/java/me/staek/nonblocking/completablefuture/complete/_03_CompleteExceptionally.java
@@ -0,0 +1,39 @@
+package me.staek.nonblocking.completablefuture.complete;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * complete - CompleteExceptionally
+ */
+public class _03_CompleteExceptionally {
+    public static void main(String[] args) {
+
+        CompletableFuture<String> cf1 = new CompletableFuture<>();
+        getData(cf1);
+        CompletableFuture<String> cf2 = cf1
+                .thenApply(result -> {
+                    System.out.println(result);
+                    return result.toUpperCase();
+                })
+                .handle((r, e) -> {
+                    if (e != null) {
+                        System.err.println("Exception: " + e.getMessage());
+                        return "noname";
+                    }
+                    return r;
+                });
+
+        System.out.println("result: " + cf2.join());
+    }
+
+    private static void getData(CompletableFuture cf) {
+        try {
+            System.out.println("비동기 작업 수행 중..");
+            Thread.sleep(500);
+//            throw new IllegalArgumentException("error");
+        } catch (Exception e) {
+            cf.completeExceptionally(e);
+        }
+        cf.complete("Hello World");
+    }
+}
