458a49d523c562ba561bffc2eef9bfbd70409c44
seongtaekkim <chxortnl@gmail.com>
2024-05-08 10:03:02 +0900

    complete - isCompletedExceptionally, isCancelled

---
+++ concurrency-practice/src/main/java/me/staek/nonblocking/completablefuture/complete/_05_isCompletedExceptionallyAndIsCancelled.java
@@ -0,0 +1,38 @@
+package me.staek.nonblocking.completablefuture.complete;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * complete - isCompletedExceptionally, isCancelled
+ */
+public class _05_isCompletedExceptionallyAndIsCancelled {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+
+        CompletableFuture<Integer> cf1 = CompletableFuture.supplyAsync(() -> 10);
+        CompletableFuture<Integer> cf2 = CompletableFuture.supplyAsync(() -> {
+//            return 20;
+            throw new RuntimeException("error");
+        });
+
+//        cf2.cancel(true);
+
+        CompletableFuture<Integer> combinedFuture =
+                cf1.thenCombine(cf2.exceptionally(e -> 15), (result1, result2) -> {
+
+                    if (cf2.isCancelled()) {
+                        return 0; // 취소 완료
+                    } else if (cf2.isCompletedExceptionally()) {
+                        return result2; // 예외 완료
+                    } else if (cf2.isDone()) {
+                        return result1 + result2; // 정상 완료
+                    } else return -1;
+
+                });
+
+        int result = combinedFuture.join(); // 결과 가져오기
+        System.out.println("최종 결과: " + result);
+    }
+}
